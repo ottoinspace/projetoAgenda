2f12fc1bda84f5a1424530ad402769383c9d9fa7
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-07-31 15:28:07 -0300

    criando acesso do usuario

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./scr/controllers/contatoController');
 
 // rotas da home
 route.get('/',homeController.index);
@@ -10,5 +11,10 @@ route.get('/',homeController.index);
 // rotas de login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+//rotas de contatos
+route.get('/contato/index', contatoController.index);
  
 module.exports = route;
